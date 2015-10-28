9e5a021f4a4305745f6a006c8e912b34fe84b261
Rik Willems <rik@metmeer.nl>
2015-10-28 13:15:40 +0100

    Update Readme and add script

---
+++ PATCH_SUPEE-6788.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+
+# What is the version number?
+version=`php -r "require 'app/Mage.php'; echo Mage::getVersion();"`
+echo "Magento version:" $version
+naked_version="${version//\./}"
+
+
+# Which patch file do we need?
+if [ $naked_version -ge 1700 -a $naked_version -lt 1702 ]; then
+	filename="1.7.0.1"
+elif [ $naked_version -ge 1702 -a $naked_version -lt 1800 ]; then
+	filename="1.7.0.2"
+elif [ $naked_version -ge 1800 -a $naked_version -lt 1810 ]; then
+	filename="1.8.0.0"
+elif [ $naked_version -ge 1810 -a $naked_version -lt 1900 ]; then
+	filename="1.8.1.0"
+elif [ $naked_version -ge 1900 -a $naked_version -lt 1901 ]; then
+	filename="1.9.0.0"
+elif [ $naked_version -ge 1901 -a $naked_version -lt 1910 ]; then
+	filename="1.9.0.1"
+elif [ $naked_version -ge 1910 -a $naked_version -lt 1911 ]; then
+	filename="1.9.1.0"
+elif [ $naked_version -ge 1911 -a $naked_version -lt 1920 ]; then
+	filename="1.9.1.1"
+elif [ $naked_version -ge 1920 -a $naked_version -lt 1921 ]; then
+	filename="1.9.2.0"
+elif [ $naked_version -ge 1921 -a $naked_version -lt 1922 ]; then
+	filename="1.9.2.1"
+else
+	filename="no-version"
+fi;
+
+
+# Download the right patch.
+if [ $filename != "no-version" ]; then
+	patch_file="PATCH_SUPEE-6788_CE_"$filename"_v1.sh"
+	echo "Download patch: $patch_file"
+	wget --no-check-certificate --quiet - https://raw.githubusercontent.com/rikwillems/SUPEE-6788/master/$patch_file
+else 
+	echo "Version not supported for patch."
+	exit;
+fi;
+
+
+# Disable and clear compiler for ease of use.
+echo "Disable compiler:"
+php -f shell/compiler.php disable
+
+echo "Clear compiler:"
+php -f shell/compiler.php clear
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # SUPEE-6788
+
+This script determines the Magento version and downloads the right patch file. For ease of use it also disables and clears compilation. After this take the normal steps for applying Magento patches.
+
+## Usage
+
+```
+bash < <(wget -q --no-check-certificate -O - https://raw.githubusercontent.com/rikwillems/SUPEE-6788/master/PATCH_SUPEE-6788.sh)
+```
